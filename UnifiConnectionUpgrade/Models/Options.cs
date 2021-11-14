9dd2363231a5ee4dc459f27a93265b5ed756770a
Jarrett Gilliam <jarrettgilliam@gmail.com>
2021-11-14 14:37:58 -0600

    Code complete

---
+++ UnifiConnectionUpgrade/Models/Options.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace UnifiConnectionUpgrade.Models
+{
+    internal class Options
+    {
+        [Option('b', "base-uri", Required = true, HelpText = "The base URI to the UniFi controller.")]
+        public Uri BaseUri { get; set; }
+
+        [Option('u', "username", Required = true, HelpText = "The username to log in with.")]
+        public string Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "The password to log in with.")]
+        public string Password { get; set; }
+
+        [Option('m', "minimum-signal-strength", Default = -70, HelpText = "The minimum signal strength to force a client to reconnect.")]
+        public int? MinimumSignalStrength { get; set; }
+
+        [Option('i', "insecure-tls", Default = false, HelpText = "Skips TLS validation.")]
+        public bool InsecureTLS { get; set; }
+
+        [Option('e', "exclude-mac", HelpText = "A list of MAC addresses to exclude. Useful for clients that only support 2.4 GHz WiFi.")]
+        public IEnumerable<string> ExcludedMacs { get; set; }
+    }
+}
--- UnifiConnectionUpgrade/Program.cs
+++ UnifiConnectionUpgrade/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandLine;
+using KoenZomers.UniFi.Api.Responses;
+using UnifiConnectionUpgrade.Models;
 
 namespace UnifiConnectionUpgrade
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(ReconnectClients);
+        }
+
+        static async Task ReconnectClients(Options options)
+        {
+            try
+            {
+                var uniFiApi = new KoenZomers.UniFi.Api.Api(options.BaseUri);
+
+                if (options.InsecureTLS)
+                {
+                    uniFiApi.DisableSslValidation();
+                }
+
+                if (!await uniFiApi.Authenticate(options.Username, options.Password))
+                {
+                    throw new Exception("Unable to authenticate.");
+                }
+
+                foreach (Clients client in await uniFiApi.GetActiveClients())
+                {
+                    if (ShouldReconnect(client, options))
+                    {
+                        await uniFiApi.ReconnectClient(client.MacAddress);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                Environment.Exit(1);
+            }
+        }
+
+        static bool ShouldReconnect(Clients client, Options options)
+        {
+            if (client == null)
+            {
+                return false;
+            }
+
+            if (client.IsWired == true)
+            {
+                return false;
+            }
+
+            if (client.RadioProtocol != "ng")
+            {
+                return false;
+            }
+
+            if (client.SignalStrength < options.MinimumSignalStrength)
+            {
+                return false;
+            }
+
+            if (options.ExcludedMacs?.Any(s => string.Equals(s, client.MacAddress, StringComparison.OrdinalIgnoreCase)) == true)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
