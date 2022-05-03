a3c5e8bd8c10ce22a2e1c29779669f16057a1a9a
Jarrett Gilliam <jarrettgilliam@gmail.com>
2022-05-02 20:24:28 -0500

    Upgrade to net6.0

--- UnifiConnectionUpgrade/Program.cs
+++ UnifiConnectionUpgrade/Program.cs
@@ -1,79 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CommandLine;
 using KoenZomers.UniFi.Api.Responses;
 using UnifiConnectionUpgrade.Models;
 
-namespace UnifiConnectionUpgrade
+namespace UnifiConnectionUpgrade;
+
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(ReconnectClients);
-        }
+        await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(ReconnectClients);
+    }
 
-        static async Task ReconnectClients(Options options)
+    static async Task ReconnectClients(Options options)
+    {
+        try
         {
-            try
+            var uniFiApi = new KoenZomers.UniFi.Api.Api(options.BaseUri);
+
+            if (options.InsecureTLS)
             {
-                var uniFiApi = new KoenZomers.UniFi.Api.Api(options.BaseUri);
+                uniFiApi.DisableSslValidation();
+            }
 
-                if (options.InsecureTLS)
-                {
-                    uniFiApi.DisableSslValidation();
-                }
+            if (!await uniFiApi.Authenticate(options.Username, options.Password))
+            {
+                throw new Exception("Unable to authenticate.");
+            }
 
-                if (!await uniFiApi.Authenticate(options.Username, options.Password))
+            foreach (Clients client in await uniFiApi.GetActiveClients())
+            {
+                if (ShouldReconnect(client, options))
                 {
-                    throw new Exception("Unable to authenticate.");
-                }
-
-                foreach (Clients client in await uniFiApi.GetActiveClients())
-                {
-                    if (ShouldReconnect(client, options))
-                    {
-                        await uniFiApi.ReconnectClient(client.MacAddress);
-                    }
+                    await uniFiApi.ReconnectClient(client.MacAddress);
                 }
             }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine(ex);
-                Environment.Exit(1);
-            }
         }
-
-        static bool ShouldReconnect(Clients client, Options options)
+        catch (Exception ex)
         {
-            if (client == null)
-            {
-                return false;
-            }
-
-            if (client.IsWired == true)
-            {
-                return false;
-            }
-
-            if (client.RadioProtocol != "ng")
-            {
-                return false;
-            }
-
-            if (client.SignalStrength < options.MinimumSignalStrength)
-            {
-                return false;
-            }
-
-            if (options.ExcludedMacs?.Any(s => string.Equals(s, client.MacAddress, StringComparison.OrdinalIgnoreCase)) == true)
-            {
-                return false;
-            }
-
-            return true;
+            Console.Error.WriteLine(ex);
+            Environment.Exit(1);
         }
+    }
+
+    static bool ShouldReconnect(Clients client, Options options)
+    {
+        if (client == null)
+        {
+            return false;
+        }
+
+        if (client.IsWired == true)
+        {
+            return false;
+        }
+
+        if (client.RadioProtocol != "ng")
+        {
+            return false;
+        }
+
+        if (client.SignalStrength < options.MinimumSignalStrength)
+        {
+            return false;
+        }
+
+        if (options.ExcludedMacs?.Any(s => string.Equals(s, client.MacAddress, StringComparison.OrdinalIgnoreCase)) == true)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
